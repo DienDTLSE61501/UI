520c1dcb021823a5bed35a40b8baed6ada50952b
Điền <diendtlse61501@fpt.edu.vn>
2018-01-30 11:58:19 +0700

    test5

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -8,7 +8,7 @@ import { SimpleLayoutComponent } from './layouts/simple-layout.component';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'dashboard',
+    redirectTo: 'pages',
     pathMatch: 'full',
   },
   {
@@ -56,7 +56,7 @@ export const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
